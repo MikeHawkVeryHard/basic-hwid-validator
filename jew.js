561b75697819cc6f9ead8f912ea50bb4d954d19e
KYS <79298966+MikeHawkVeryHard@users.noreply.github.com>
2021-02-19 09:46:54 +0000

    Update jew.js

--- jew.js
+++ jew.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const Joi = require('@hapi/joi');
 const who = express();
+const bodyParser = require('body-parser');
+
+who.use(bodyParser.json())
+who.use(bodyParser.urlencoded({
+    limit: '2mb',
+    extended: false
+}))
 
 who.post('/auth', async (req, res) => {
-    const {error, value} = Joi.object({hwid: Joi.string().required()})
+    const {error, value} = Joi.object({hwid: Joi.string().required()}).validate(req.body)
     if(error) return res.status(400).send('Bad request you dumb cunt')
     const Validate_HWID = value.hwid.match(/[0-9,A-z]{8}-[0-9,A-z]{4}-[0-9,A-z]{4}-[0-9,A-z]{4}-[0-9,A-z]{12}/);
     if(!Validate_HWID) return res.status(401).send(`Aw shit looks like you're a dumb cunt`)
